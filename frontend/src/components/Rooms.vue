b46840f1cf6686f97fcf014a17ded5bcedd63da9
Shane O'Moore <shane.omoore@livetiles.nyc>
2020-03-31 09:19:18 +0100

    generate quiz & store in local db, start creating ui for rooms view

--- frontend/src/components/Rooms.vue
+++ frontend/src/components/Rooms.vue
@@ -1,31 +1,64 @@
 <template>
   <div class="rooms">
-    Hi, {{ username }}
-    <div v-if="database.rooms.length === 0">
-      <p>You have no quiz rooms setup</p>
-    </div>
-
-    <div v-if="database.rooms.length >= 1">
-      <strong>Quiz Rooms</strong>
-      <ul>
-        <li v-for="room in database.rooms" :key="room.id">
-          {{ room.name }} - {{ room.id }}
-        </li>
-      </ul>
-    </div>
-
-    <div>
-      <input
-        class="input"
-        type="text"
-        v-model="newQuizRoomName"
-        placeholder="New Quiz Room"
-        @keypress.enter="createDatabase(newQuizRoomName)"
-      />
-      <button class="button" @click="createDatabase(newQuizRoomName)">
-        create
-      </button>
-      <button class="button">join</button>
+    <div class="tile is-ancestor" style="margin:1em;">
+      <div class="tile is-parent">
+        <article class="tile is-child box">
+          <p class="title">Quiz Rooms</p>
+          <hr />
+          <div class="content">
+            <div v-if="database.rooms.length >= 1">
+              <div v-for="room in database.rooms" :key="room.id">
+                <h4 class="title is-5">{{ room.name }}</h4>
+                <button class="button">Remove</button>
+                <br />
+                <br />
+              </div>
+            </div>
+            <div v-else>
+              No Quiz Rooms :(
+            </div>
+          </div>
+        </article>
+      </div>
+      <div class="tile is-parent is-8">
+        <article class="tile is-child box">
+          <p class="title">Main column</p>
+          <hr />
+          <div class="content">
+            <div>
+              <input
+                class="input"
+                type="text"
+                v-model="newQuizRoomName"
+                placeholder="New Quiz Room"
+                @keypress.enter="createDatabase(newQuizRoomName)"
+              />
+              <br />
+              <br />
+              <button
+                class="button is-fullwidth is-primary is-outlined"
+                @click="createDatabase(newQuizRoomName)"
+              >
+                Create
+              </button>
+              <br />
+              <br />
+              <input
+                class="input"
+                type="text"
+                v-model="newQuizRoomName"
+                placeholder="New Quiz Room"
+                @keypress.enter="createDatabase(newQuizRoomName)"
+              />
+              <br />
+              <br />
+              <button class="button is-fullwidth is-primary is-outlined">
+                Join
+              </button>
+            </div>
+          </div>
+        </article>
+      </div>
     </div>
   </div>
 </template>
@@ -33,15 +66,29 @@
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
 import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
 
 interface Room {
   id: string;
   name: string;
+  quiz: Quiz;
 }
 
 interface Database {
   username: string;
   rooms: Room[];
+}
+
+interface Question {
+  question: string;
+  difficulty: string;
+  answers: string[];
+  correctAnswer: string;
+}
+
+interface Quiz {
+  category: string;
+  questions: Question[];
 }
 
 @Component
@@ -50,6 +97,8 @@ export default class Login extends Vue {
 
   private newQuizRoomName = "";
   database: Database | null = null;
+
+  private api = `https://opentdb.com/api.php?amount=10&category=9&type=multiple`;
 
   created(): void {
     this.database = this.getDatabase(this.username);
@@ -80,59 +129,86 @@ export default class Login extends Vue {
 
   createDatabase(roomName: string): void {
     this.newQuizRoomName = "";
-    const newRoom: Room = {
-      name: roomName,
-      id: uuidv4()
-    };
-    const strDb: string | null = window.localStorage.getItem("trivially");
 
-    if (!strDb) {
-      window.localStorage.setItem(
-        "trivially",
-        JSON.stringify({
-          databases: [
-            {
+    this.generateQuiz().then(quiz => {
+      const newRoom: Room = {
+        name: roomName,
+        id: uuidv4(),
+        quiz
+      };
+      const strDb: string | null = window.localStorage.getItem("trivially");
+
+      if (!strDb) {
+        window.localStorage.setItem(
+          "trivially",
+          JSON.stringify({
+            databases: [
+              {
+                username: this.username,
+                rooms: [newRoom]
+              }
+            ]
+          })
+        );
+      } else {
+        const parsed: { databases: Database[] } | undefined = JSON.parse(strDb);
+
+        if (parsed) {
+          const userDatabase = parsed.databases.find(
+            database => database.username === this.username
+          );
+
+          if (!userDatabase) {
+            parsed.databases.push({
               username: this.username,
               rooms: [newRoom]
-            }
-          ]
-        })
-      );
-    } else {
-      const parsed: { databases: Database[] } | undefined = JSON.parse(strDb);
+            });
 
-      if (parsed) {
-        const userDatabase = parsed.databases.find(
-          database => database.username === this.username
-        );
+            window.localStorage.setItem("trivially", JSON.stringify(parsed));
+          } else {
+            parsed.databases.forEach(db => {
+              if (db.username === this.username) {
+                db.rooms.push(newRoom);
+              }
+            });
+            window.localStorage.setItem("trivially", JSON.stringify(parsed));
+          }
 
-        if (!userDatabase) {
-          parsed.databases.push({
-            username: this.username,
-            rooms: [newRoom]
-          });
-
-          window.localStorage.setItem("trivially", JSON.stringify(parsed));
-        } else {
-          parsed.databases.forEach(db => {
-            if (db.username === this.username) {
-              db.rooms.push(newRoom);
-            }
-          });
-          window.localStorage.setItem("trivially", JSON.stringify(parsed));
-        }
-
-        if (this.database) {
-          this.database.rooms = [...this.database.rooms, { ...newRoom }];
+          if (this.database) {
+            this.database.rooms = [...this.database.rooms, { ...newRoom }];
+          }
         }
       }
+    });
+  }
+
+  async generateQuiz(): Promise<Quiz> {
+    try {
+      const response = await axios.get(this.api);
+      const category = response.data.results[0]["category"];
+      const questions: Question[] = response.data.results.map(
+        (obj: {
+          question: string;
+          difficulty: string;
+          incorrect_answers: string[];
+          correct_answer: string;
+        }) => ({
+          question: obj.question,
+          difficulty: obj.difficulty,
+          answers: [...obj.incorrect_answers, obj.correct_answer],
+          correctAnswer: obj.correct_answer
+        })
+      );
+
+      const quiz = {
+        category,
+        questions
+      };
+
+      return quiz;
+    } catch (error) {
+      throw new Error(error);
     }
-
-    // const db: string | undefined = JSON.stringify(json);
-
-    // if (db) {
-    //   window.localStorage.setItem("trivially", db);
-    // }
   }
 }
 </script>
